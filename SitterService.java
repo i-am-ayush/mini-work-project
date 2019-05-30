63cd8ae518081445c55c7467ed67b0cef570f32b
i-am-ayush <46191140+i-am-ayush@users.noreply.github.com>
2019-05-31 02:32:44 +0530

    Update SitterService.java

--- SitterService.java
+++ SitterService.java
@@ -1,7 +1,16 @@
 package service;
 
+import bean.Application;
+import bean.Job;
 import bean.Sitter;
+import dao.ApplicationDao;
+import dao.JobDao;
 import dao.SitterDao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SitterService {
     public static Sitter getSitterById(int id){
@@ -11,13 +20,14 @@ public class SitterService {
     public static boolean registerSitter(Sitter sitter){
         return SitterDao.save(sitter);
     }
-        public static List<Map<String, Object>> allAppliedJobBySitter(int memberId){
+    public static List<Map<String, Object>> allAppliedJobBySitter(int memberId){
         List<Application> listApplication= ApplicationDao.getAllApplicationById(memberId);
         List<Job> listJob=new ArrayList<>();
         List<Map<String, Object>> listMap=new ArrayList<>();
         for(int i=0;i<listApplication.size();i++){
             listJob.add(JobDao.getById(listApplication.get(i).getJobId()));
             Map<String,Object> tempMap=new HashMap<>();
+            tempMap.put("jobId",JobDao.getById(listApplication.get(i).getJobId()) );
             tempMap.put("title",JobDao.getById(listApplication.get(i).getJobId()).getTitle() );
             tempMap.put("payPerHour",JobDao.getById(listApplication.get(i).getJobId()).getPayPerHour());
             tempMap.put("status", listApplication.get(i).getStatus());
@@ -27,5 +37,18 @@ public class SitterService {
         return listMap;
 
 
+    }
+
+    public static void main(String[] args) {
+        List<Map<String, Object>> l = new ArrayList<>();
+
+        l = SitterService.allAppliedJobBySitter(5);
+        for (int i = 0; i < l.size(); i++) {
+            System.out.println("------------neww job-----------");
+            for (Map.Entry<String, Object> entry : l.get(i).entrySet()) {
+                System.out.println("Key = " + entry.getKey() +
+                        ", Value = " + entry.getValue());
+            }
+        }
     }
 }
