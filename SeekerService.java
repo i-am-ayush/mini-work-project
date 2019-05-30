64bbd6dc2349bc3d56c1f375a5ff4fc82723ac17
i-am-ayush <46191140+i-am-ayush@users.noreply.github.com>
2019-05-31 00:20:16 +0530

    Update SeekerService.java

--- SeekerService.java
+++ SeekerService.java
@@ -1,7 +1,11 @@
 package service;
 
+import bean.Job;
 import bean.Seeker;
+import dao.JobDao;
 import dao.SeekerDao;
+
+import java.util.List;
 
 public class SeekerService {
 public static Seeker getSeekerById(int id){
@@ -12,4 +16,15 @@ public static boolean registerSeeker(Seeker seeker){
     boolean result=SeekerDao.save(seeker);
     return result;
 }
+
+public static List<Job> allJobPostedBySeeker(int memberId){
+    return JobDao.getJobsPostedBy(memberId);
+}
+
+    public static void main(String[] args) {
+        List<Job> l=SeekerService.allJobPostedBySeeker(2);
+        for(int i=0;i<l.size();i++){
+            System.out.println(l.get(i).getTitle());
+        }
+    }
 }
