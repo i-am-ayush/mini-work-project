3b9fdd4846a4ac87418765e373b83060d0f9cfce
i-am-ayush <46191140+i-am-ayush@users.noreply.github.com>
2019-05-30 23:44:20 +0530

    Update SitterService.java

--- SitterService.java
+++ SitterService.java
@@ -11,4 +11,21 @@ public class SitterService {
     public static boolean registerSitter(Sitter sitter){
         return SitterDao.save(sitter);
     }
+        public static List<Map<String, Object>> allAppliedJobBySitter(int memberId){
+        List<Application> listApplication= ApplicationDao.getAllApplicationById(memberId);
+        List<Job> listJob=new ArrayList<>();
+        List<Map<String, Object>> listMap=new ArrayList<>();
+        for(int i=0;i<listApplication.size();i++){
+            listJob.add(JobDao.getById(listApplication.get(i).getJobId()));
+            Map<String,Object> tempMap=new HashMap<>();
+            tempMap.put("title",JobDao.getById(listApplication.get(i).getJobId()).getTitle() );
+            tempMap.put("payPerHour",JobDao.getById(listApplication.get(i).getJobId()).getPayPerHour());
+            tempMap.put("status", listApplication.get(i).getStatus());
+            tempMap.put("expectedPay", listApplication.get(i).getExpectedPay());
+            listMap.add(tempMap);
+        }
+        return listMap;
+
+
+    }
 }
